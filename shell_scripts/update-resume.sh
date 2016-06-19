9b9136c76133f4b3b2f7be672e7f5fe888f1d02d
Samuel Fahlberg <sam.fahlberg@gmail.com>
2016-06-18 19:05:46 -0700

    add shell scripts to dotfiles, move bb config

---
+++ shell_scripts/update-resume.sh
@@ -0,0 +1,11 @@
+update-resume() {
+# copy to google drive
+  cp ~/Downloads/sam_fahlberg_resume.docx.pdf ~/Google\ Drive/search/sam_fahlberg_resume.pdf
+# move to website and push changes
+  mv ~/Downloads/sam_fahlberg_resume.docx.pdf ~/Documents/sfahlberg.github.io/resume.pdf
+  cd ~/Documents/sfahlberg.github.io
+  git add -A
+  git commit -m "update resume"
+  git push
+  cd
+}
