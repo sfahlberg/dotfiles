2ea676afd4da35e5158d0421b8af78b9904dd87a
Samuel Fahlberg <sam.fahlberg@gmail.com>
2016-07-17 15:31:24 -0700

    move general config to new file

--- shell_scripts/bb_config.sh
+++ shell_scripts/bb_config.sh
@@ -1,5 +1,4 @@
 if [ $(hostname) = "sam-workbook.local" ]; then
-  eval "$(rbenv init -)"
   source ~/bb/brightbytes/.env
   export PATH=$PATH:/Applications/Postgres.app/Contents/Versions/latest/bin
 fi
---
+++ shell_scripts/general_config.sh
@@ -0,0 +1,6 @@
+export PATH="$HOME/.rbenv/bin:$PATH"
+eval "$(rbenv init -)"
+
+alias vi="vim"
+
+export BROWSER=google-chrome-stable
