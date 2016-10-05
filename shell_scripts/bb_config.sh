85d73d168fd1d61fd08642a85a23e4ec650e4e1b
Samuel Fahlberg <sam.fahlberg@gmail.com>
2016-10-05 15:56:41 -0700

    add go config

--- shell_scripts/bb_config.sh
+++ shell_scripts/bb_config.sh
@@ -2,5 +2,6 @@ if [ $(hostname) = "sam-workbook.local" ]; then
   alias bbv="cd ~/bb/brightbytes; vi"
   alias bb="cd ~/bb/brightbytes"
   source ~/bb/brightbytes/.env
-  export PATH=$PATH:/Applications/Postgres.app/Contents/Versions/latest/bin
+  export GOPATH=$HOME/goworkspace/
+  export PATH=$PATH:/Applications/Postgres.app/Contents/Versions/latest/bin:$GOPATH/bin
 fi
