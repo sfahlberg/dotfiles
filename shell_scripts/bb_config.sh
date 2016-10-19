34c0a5c4d0284e304d04de6fc9fb103cd2d965a5
Samuel Fahlberg <sam.fahlberg@gmail.com>
2016-10-19 15:44:47 -0700

    use node_modules for eslint

--- shell_scripts/bb_config.sh
+++ shell_scripts/bb_config.sh
@@ -4,4 +4,5 @@ if [ $(hostname) = "sam-workbook.local" ]; then
   source ~/bb/brightbytes/.env
   export GOPATH=$HOME/goworkspace/
   export PATH=$PATH:/Applications/Postgres.app/Contents/Versions/latest/bin:$GOPATH/bin
+  export PATH=$PATH:/Users/sam/bb/brightbytes/node_modules/.bin
 fi
