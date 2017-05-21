9da29fd9d3edfaf83ff41b6121c27c4e8a80d425
Samuel Fahlberg <sam.fahlberg@gmail.com>
2017-05-21 13:21:36 -0700

    make rpi use tmux

--- shell_scripts/rpi_config.sh
+++ shell_scripts/rpi_config.sh
@@ -1,3 +1,12 @@
 if [ $(hostname) = "rpi" ]; then
   alias sudo='sudo -E' #to preserve the environment (vimrc, etc)
+
+  export NVM_DIR="$HOME/.nvm"
+  [ -s "$NVM_DIR/nvm.sh"  ] && . "$NVM_DIR/nvm.sh" # This loads nvm
+
+  # Auto start a session when Zsh is launched in a local terminal.
+  zstyle ':prezto:module:tmux:auto-start' local 'yes'
+
+  # Auto start a session when Zsh is launched in a SSH connection.
+  zstyle ':prezto:module:tmux:auto-start' remote 'yes'
 fi
